61641bca4ac1a12f2dbf4fff4bffa6b58bc50a38
DiddierKantun <Diddier.Kantun@theksquaregroup.com>
2021-09-20 02:40:28 -0500

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,135 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
 
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(16.0),
+            height: 180,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        backgroundImage: AssetImage("assets/img/profile.png"),
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text("Jhon Doe",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold)),
+                            Text("28 years. Male",
+                                style: TextStyle(
+                                    fontSize: 14, color: Colors.grey)),
+                          ],
+                        ),
+                      ),
+                      const Icon(Icons.more_vert, color: Colors.blue),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '+52 999 566 1263',
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left,
+                          ),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            'Contact.',
+                            style: TextStyle(fontSize: 13, color: Colors.blue),
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: const [
+                      Text(
+                        '07:00 PM',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(
+                        width: 8.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'September 02, 2021.',
+                          style: TextStyle(fontSize: 13, color: Colors.grey),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      Text(
+                        'Consult',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
